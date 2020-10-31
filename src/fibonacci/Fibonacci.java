66e89aa7b659e6a27a30efb5d328600ff36c3bce
Przemek <przemyslawjakubowski@gmail.com>
2020-10-31 15:29:39 +0100

    Fibonacci polecenie

---
+++ src/fibonacci/Fibonacci.java
@@ -0,0 +1,10 @@
+package fibonacci;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        // Ciąg Fibonacciego:
+        // https://pl.wikipedia.org/wiki/Ci%C4%85g_Fibonacciego
+        //
+        // Napisz program podający wartość ciągu fibonacciego z zadanej pozycji
+    }
+}
