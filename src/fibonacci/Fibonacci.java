799589c8a01b950664b0967f64d9b3dc081114dc
Przemek <przemyslawjakubowski@gmail.com>
2020-10-31 15:35:20 +0100

    Fibonacci rozwiazanie

--- src/fibonacci/Fibonacci.java
+++ src/fibonacci/Fibonacci.java
@@ -1,10 +1,20 @@
 package fibonacci;
 
+import java.util.Scanner;
+
 public class Fibonacci {
     public static void main(String[] args) {
-        // Ciąg Fibonacciego:
-        // https://pl.wikipedia.org/wiki/Ci%C4%85g_Fibonacciego
-        //
-        // Napisz program podający wartość ciągu fibonacciego z zadanej pozycji
+        System.out.println("Ktory wyraz ciagu podac?");
+        Scanner in = new Scanner(System.in);
+        int numerWCiagu = in.nextInt();
+
+        int x1 = 1;
+        int x2 = 1;
+        for (int i = 3; i <= numerWCiagu; i++) {
+            int temp = x1 + x2;
+            x1 = x2;
+            x2 = temp;
+        }
+        System.out.println(x2);
     }
 }
