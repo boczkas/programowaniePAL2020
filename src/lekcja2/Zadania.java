8ec28ad1cf681272327b15b7d3d2bcb581c42821
Przemek <przemyslawjakubowski@gmail.com>
2020-11-18 18:12:09 +0100

    Lekcja 2 aktualizacja

--- src/lekcja2/Zadania.java
+++ src/lekcja2/Zadania.java
@@ -9,11 +9,21 @@ public class Zadania {
         String imie = "Przemek";
         System.out.println(imie);
 
+//        2.
+        int pierwsza = 1;
+        int druga = 2;
+
+        int wynik = pierwsza + druga;
+
+        System.out.println(wynik);
+
+//        5.
+        System.out.println(Math.PI);
     }
 //    W kodzie używaj zmiennych do przechowywania wartości
 //    1. Napisz kod, który wypiszę Twoje imię
 //    2. Napisz kod, który doda to siebie dwie liczby i wypisze wynik dodawania
 //    3. Napisz kod, który podzieli dwie liczby i wypisze wynik dzielenia
-//    4. Napisz kod, który dzieli mnoży liczby
+//    4. Napisz kod, który mnoży liczby
 //    5. Napisz kod, który oblicza pole koła o promieniu 5
 }
