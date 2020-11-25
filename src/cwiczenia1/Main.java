c5868831dd2d6ee78c2a18674b02e6f52d464571
Przemek <przemyslawjakubowski@gmail.com>
2020-11-25 14:15:56 +0100

    Cwiczenia 1

---
+++ src/cwiczenia1/Main.java
@@ -0,0 +1,20 @@
+package cwiczenia1;
+
+public class Main {
+    public static void main(String[] args) {
+        // 1. Stwórz 4 zmienne przechowujące dowolne słowa. Wypisz je w jednej linijce (pamiętaj o przerwach między słowami)
+        //
+        // 2. Oblicz program liczący pole prostokąta. Wypisz tą wartość. Wartości a i b przechowuj w zmiennych.
+        //
+        // 3. Oblicz program liczący pole trójkąta. Wypisz tą wartość. Wartości a i h przechowuj w zmiennych.
+        //
+        // 3. Napisz program liczący długość przeciwprostokątnej w trójkącie prostokątnym. Wypisz tą wartość.
+        //    Długości przyprostokątnych przechowuj w zmiennych
+        //    Math.sqrt(liczba) - obliczanie pierwiastka z danej liczby
+        //    Math.pow(liczba, potega) - obliczanie potęgi danej liczby
+        // 4.
+        // Zadanie domowe:
+        // Napisz program rozwiązujący równania kwadratowe (o ile istnieje)
+        // https://www.matemaks.pl/rownania-kwadratowe-w-postaci-ogolnej.html
+    }
+}
